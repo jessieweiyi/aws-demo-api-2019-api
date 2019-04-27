eef115b4a3ec42efbd9b3aaffe69ca8415c84632
Jessie Wei <jwei@seek.com.au>
2019-04-27 12:11:52 +1000

    Added error handling

--- src/api/job/queryJob.js
+++ src/api/job/queryJob.js
@@ -3,13 +3,19 @@ import AWSServiceFactory from '../../services/awsServiceFactory';
 function queryJob(req, res, next) {
   const dbService = AWSServiceFactory.getDBService();
   const { jobId } = req.params;
-  return dbService.getJob(jobId).then((result) => {
-    if (result) {
-      res.status(200).json(result);
-    } else {
-      res.status(204);
-    }
-  }).then(next);
+  return dbService.getJob(jobId)
+    .then((result) => {
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        res.status(204);
+      }
+    })
+    .catch(error => {
+      logger.error(error);
+      res.status(500);
+    })
+    .then(next);
 }
 
 module.exports = queryJob;
