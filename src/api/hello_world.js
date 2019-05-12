bdd6a042bae613d0f1c0997845e6aa68eed4c2d6
Jessie Wei <jwei@seek.com.au>
2019-05-12 21:17:42 +1000

    Updated hello world endpoint

--- src/api/hello_world.js
+++ src/api/hello_world.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.status(200).send('Hello World - V2\n');
+  res.status(200).send('Hello World - V3\n');
 });
 
 module.exports = router;
